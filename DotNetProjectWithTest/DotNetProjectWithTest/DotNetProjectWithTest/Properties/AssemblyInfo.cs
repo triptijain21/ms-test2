96b52e3c83db8d7f817f9ec4e610de19b2fa4eb1
Tripti Jain <triptijain@microsoft.com>
2022-02-22 16:51:36 +0530

    commit

---
+++ DotNetProjectWithTest/DotNetProjectWithTest/DotNetProjectWithTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DotNetProjectWithTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DotNetProjectWithTest")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("86c557c0-fb49-4bc6-a47f-9725d306f515")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ DotNetProjectWithTest/DotNetProjectWithTest/DotNetProjectWithTest/UnitTestSample.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace DotNetProjectWithTest
+{
+    [TestClass]
+    public class UnitTestSample
+    {
+        [TestMethod]
+        public void CheckEqualityTestPass()
+        {
+            int a = 2, b = 2;
+            Assert.AreEqual(a, b);
+        }
+
+        [TestMethod]
+        public void CheckEqualityTestFail()
+        {
+            int a = 3, b = 2;
+            Assert.AreEqual(a, b);
+        }
+
+        [TestMethod]
+        public void CheckTrueTestPass()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod]
+        public void CheckTrueTestFail()
+        {
+            Assert.IsTrue(false);
+        }
+    }
+}
